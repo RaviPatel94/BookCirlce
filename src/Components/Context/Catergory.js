03fb82257777584eec5735fca58e2046a3c24940
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2025-01-01 11:12:18 +0530

    dynamic compoenet

---
+++ src/Components/Context/Catergory.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+let categorycontext=React.createContext()
+
+export default categorycontext
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -2,11 +2,13 @@ import React, {useEffect,useState} from 'react'
 import axios from 'axios';
 
 function Menu() {
-
+  const apikey = import.meta.env.VITE_BOOK;
   const [data, setdata] = useState([])
   const getmainbooks= async()=>{
     try {
-      const res=await axios.get("https://www.googleapis.com/books/v1/volumes?q=book&maxResults=40&startIndex=0&key=AIzaSyBM7n0PHnpqWbNL95Qaz0HWD-zjoru-yWk").then(setdata(res.data.items))   
+      const res=await axios.get(`https://www.googleapis.com/books/v1/volumes?q=book&maxResults=40&startIndex=0&key=${apikey}`)
+      setdata(res.data.items)
+      console.log(res.data.items  )
        } catch (error) {
       console.log("fetch error"+error)
     }
@@ -19,21 +21,26 @@ function Menu() {
 
   return (
     <main className='min-h-screen pt-5 px-6'>
-      <div className='grid grid-cols-6 gap-6'>
+      <div className='grid grid-cols-7 gap-6'>
         {data.map((item)=>{
-          // let thumbnail=item.volumeInfo.image
-          <div className='border-2 h-60 flex flex-col items-center'>
-          <div className='h-[185.71px] w-[130px] '>
-            <img src="/images/book1.jpg" alt="" className=' w-full h-full object-fill' />
-          </div>
-          <div className='flex flex-col items-center'>
-            <h3>the man and sheep</h3>
-            <div className='flex justify-between w-full'>
-              <div>price</div>
-              <div>Rate</div>
+          let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail
+          const randomprice = Math.floor(Math.random() * (700 - 300 + 1)) + 300;
+          if (thumbnail!=undefined){
+            return(
+              <div key={item.id} className='border-2 h-60 flex flex-col items-center'>
+              <div className='h-[185.71px] w-[130px] '>
+                <img src={thumbnail} alt="" className=' w-full h-full object-fill' />
+              </div>
+              <div className='flex flex-col items-center w-full pt-2'>
+                <h3 className=' w-11/12 text-sm truncate'>{item.volumeInfo.title}</h3>
+                <div className='flex justify-between w-11/12 text-xs'>
+                  <div>{randomprice}</div>
+                  <div>Rate</div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+              )
+          }
         })}
       </div>
         
